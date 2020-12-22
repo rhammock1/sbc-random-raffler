177d7cfc835a1afebad90e7c23d3b9ab857761ad
rhammock1 <rhamm1027@gmail.com>
2020-12-22 15:04:05 -0500

    Added api requests for get, post, delete entires for file upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import './App.css';
+import config from './config';
 
 class App extends React.Component {
+  
   state = {
     newPerson: '',
     winner: '',
     people: [],
     entries: 0,
+    uploadEntries: [],
+    csv: {},
+    formData: {}
+  }
+
+  handleUploadChange = (e) => {
+    
+    this.setState({ csv: e.target.files[0]})
+    let form = document.getElementById('form');
+    let formData = new FormData(form);
+    let csv = this.state.csv
+    formData.append('csvFile', csv);
+    this.setState({ formData: formData })
+    
+  }
+  handleUpload = async () => {
+    
+    await fetch(`${config.API_ENDPOINT}`, {
+      method: 'POST',
+      body: this.state.formData
+    })
+      .then(res => {
+        if(!res.ok) {
+          return res.json().then(e => Promise.reject(e))
+        } else {
+          return res.json();
+        }
+      })
+      .catch( error => {
+        console.error({ error })
+      })
+    await fetch(`${config.API_ENDPOINT}`)
+      .then(res => {
+        if(!res.ok) {
+          return res.json().then(e => Promise.reject(e))
+        } else {
+          return res.json()
+        }
+      })
+      .then(uploadEntries => {
+        this.setState({ uploadEntries })
+      })
   }
   
   handlePeopleSubmit = (e) => {
@@ -35,27 +79,57 @@ class App extends React.Component {
   handleWinnerSubmit = (e) => {
     // Winner submit button click
     e.preventDefault();
-    this.shuffleArray(this.state.people);
-    // let result = this.state.people[Math.floor(Math.random() * this.state.people.length)]
-    // this.setState({ winner: result })
-
     let resultArray = [];
-    this.state.people.forEach(person => {
+    if(this.state.people.length > 0) {
+      // this.shuffleArray(this.state.people);
+      this.state.people.forEach(person => {
       for(let i = 0; i < person.entries; i++) {
         resultArray.push(person.full_name);
       }
     })
+    
+    }
+    else {
+      // this.shuffleArray(this.state.uploadEntries)
+      this.state.uploadEntries.forEach(entry => {
+        for(let i = 0; i < entry.entries; i++) {
+          resultArray.push(entry.full_name);
+        }
+      })
+    }
     this.shuffleArray(resultArray);
     console.log(resultArray);
+    // let result = this.state.people[Math.floor(Math.random() * this.state.people.length)]
+    // this.setState({ winner: result })
+
+   
+    
     let result = resultArray[Math.floor(Math.random() * resultArray.length)]
     this.setState({ winner: result })
     
   };
+  handleClearData = e => {
+    e.preventDefault();
+    fetch(`${config.API_ENDPOINT}`, {
+      method: 'DELETE'
+    })
+      .then(res => {
+        if(!res.ok) {
+          return res.json().then(e => Promise.reject(e))
+        }
+        return res
+      })
+      .catch(error => {
+        console.error({ error })
+      })
+    this.setState({ uploadEntries: []})
+  }
 
   render() {
+     this.fileInput = React.createRef();
     return (
       <>
-      <form onSubmit={e => this.handlePeopleSubmit(e)}>
+      <form id='form' onSubmit={e => this.handlePeopleSubmit(e)}>
         <fieldset>
           <legend>Input values to choose random winner</legend>
           <div className='form-group'>
@@ -69,8 +143,14 @@ class App extends React.Component {
           <button type='submit'>Submit Name</button>
           <p id='or'> - or -</p>
           <div className='form-group'>
-            <label htmlFor='file-import'>Import Excel sheet: </label>
-            <input type='file' id='file-import' name='file-import' />
+            <label htmlFor='csvFile'>Import Excel sheet: </label>
+            <input onChange={this.handleUploadChange}type='file' id='csvFile' name='csvFile' ref={this.fileInput} />
+            <button type='button' onClick={e => this.handleUpload(e)} >Upload</button>
+          </div>
+          <br />
+          <div className='form-group'>
+            <label htmlFor='clear'>Clear all data from database. Must be done before each upload!</label>
+            <button type='button' onClick={e => this.handleClearData(e)}>Clear database</button>
           </div>
         </fieldset>
       </form>
